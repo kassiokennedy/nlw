ea993fe29fe56b1fec86288446f6b811a259cd65
Kassio <kassio1206@hotmail.com>
2021-10-17 21:39:08 -0300

    dia17do10-21:39

--- main.js
+++ main.js
@@ -16,3 +16,17 @@ for (const link of links) {
     nav.classList.remove('show')
   })
 }
+
+/*mudar o header com scroll */
+const header = document.querySelector('#header')
+const navHeight = header.offsetHeight
+
+window.addEventListener('scroll', function () {
+  if (window.scrollY >= navHeight) {
+    /*maior que a altura do header */
+    header.classList.add('scroll')
+  } else {
+    /*menor que a altura do header */
+    header.classList.remove('scroll')
+  }
+})
