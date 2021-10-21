a6cfa21df40b654c32a19eae4ce93d687ef76331
kassiokennedy <kassio1206@hotmail.com>
2021-10-21 10:13:20 -0300

    dia21do10-10:13

--- main.js
+++ main.js
@@ -31,3 +31,11 @@ window.addEventListener('scroll', function () {
   }
 })
 /*slider testimonials*/
+const swiper = new Swiper('.swiper-container', {
+  slidesPerView: 1,
+  pagination: {
+    el: '.swiper-pagination'
+  },
+  mousewheel: true,
+  keyboard: true
+})
